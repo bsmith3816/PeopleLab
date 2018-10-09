6bcb6416b8c87ac812d0e6b4ae06faf999d9bab5
luigi <bsmith3816@bths.edu>
2018-10-08 23:32:41 -0400

    10/8 #2

--- src/Runner.java
+++ src/Runner.java
@@ -2,7 +2,12 @@ public class Runner {
     static String[] firstNames = {"Bob","Sherry","Abraham","Donald","Goofy","Alex","Carol","Erasmus","Fa","George","Hillary","Irelia","Jacky","Kyle","Momo","Nono","Peggy","Queen","Sarah","Terry","U","Weather","Xemnas","Yolo","Zawarudo"};
     static String[] familyNames = {"Chen","Li","Lee","Shen","Qi","Chang","Chen","Jiang","Ng","Nguyen","Kim","Wong","Wang","Lin","Liu","Huang","Zhang","Zheng","Tang","Hong"};
     public static void main(String[] args){
+        Person teacher = new Teacher("Biology","Mr.","Fisher","Davis");
+        Student[] students;
 
+        for(int i = 0;i <= 30;i++){
+            students[i] = 0;
+        }
     }
 }
 
