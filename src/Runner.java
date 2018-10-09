c2c8ff01291000c208575f669580e0035916f1ea
Xander <acox9567@bths.edu>
2018-10-09 10:17:57 -0400

    sxdg

--- src/Runner.java
+++ src/Runner.java
@@ -1,13 +1,22 @@
 public class Runner {
     static String[] firstNames = {"Bob","Sherry","Abraham","Donald","Goofy","Alex","Carol","Erasmus","Fa","George","Hillary","Irelia","Jacky","Kyle","Momo","Nono","Peggy","Queen","Sarah","Terry","U","Weather","Xemnas","Yolo","Zawarudo"};
     static String[] familyNames = {"Chen","Li","Lee","Shen","Qi","Chang","Chen","Jiang","Ng","Nguyen","Kim","Wong","Wang","Lin","Liu","Huang","Zhang","Zheng","Tang","Hong"};
-    public static void main(String[] args){
-        Person teacher = new Teacher("Biology","Mr.","Fisher","Davis");
-        Student[] students;
 
-        for(int i = 0;i <= 30;i++){
-            students[i] = 0;
+    public static Student randomStudent(){
+        Student randomStudent = new Student((int)(Math.random() * 4), (int)(Math.random() * 6), (int)(Math.random() * 200), firstNames[(int)Math.random() * firstNames.length], familyNames[(int)Math.random() * familyNames.length]);
+        return randomStudent;
+    }
+
+    public static void main(String[] args){
+        Teacher teacher = new Teacher("Biology","Mr.","Fisher","Davis");
+        Student[] students = new Student[30];
+
+        for(int i = 0; i < 30; i++){
+            Student s = randomStudent();
+            students[i] = s;
         }
+
+        Classroom room = new Classroom(students, teacher);
+        room.printClass();
     }
 }
-
